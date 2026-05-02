bf871af1638aef868edfa090cde619811df915f2
Cristian Perez <cperezlecaros@gmail.com>
2026-05-02 10:59:45 -0700

    feat: implement announcements section

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from "@/components/Announcements";
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import EventCalendar from "@/components/EventCalendar";
@@ -6,7 +7,7 @@ import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
-    <div className="p-4 flex gap-4 flex-col md:flex-row">
+    <section className="p-4 flex gap-4 flex-col md:flex-row">
       {/*LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className="flex gap-4 justify-between flex-wrap">
@@ -32,8 +33,9 @@ const AdminPage = () => {
       {/*RIGHT panel*/}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendar />
+        <Announcements />
       </div>
-    </div>
+    </section>
   );
 };
 
