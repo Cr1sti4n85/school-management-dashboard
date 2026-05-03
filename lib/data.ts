8953534d4355572893c8931a58faf5f54a16ba9a
Cristian Perez <cperezlecaros@gmail.com>
2026-05-03 09:51:24 -0700

    feta: create student page, add big calendar

--- lib/data.ts
+++ lib/data.ts
@@ -112,3 +112,148 @@ export const menuItems = [
     ],
   },
 ];
+
+export const calendarEvents = [
+  {
+    title: "Math",
+    allDay: false,
+    start: new Date(2026, 4, 1, 8, 0),
+    end: new Date(2026, 4, 1, 8, 45),
+  },
+  {
+    title: "English",
+    allDay: false,
+    start: new Date(2026, 4, 1, 9, 0),
+    end: new Date(2026, 4, 1, 9, 45),
+  },
+  {
+    title: "Biology",
+    allDay: false,
+    start: new Date(2026, 4, 1, 10, 0),
+    end: new Date(2026, 4, 1, 10, 45),
+  },
+  {
+    title: "Physics",
+    allDay: false,
+    start: new Date(2026, 4, 5, 11, 0),
+    end: new Date(2026, 4, 5, 11, 45),
+  },
+  {
+    title: "Chemistry",
+    allDay: false,
+    start: new Date(2026, 4, 5, 13, 0),
+    end: new Date(2026, 4, 5, 13, 45),
+  },
+  {
+    title: "History",
+    allDay: false,
+    start: new Date(2026, 4, 5, 14, 0),
+    end: new Date(2026, 4, 5, 14, 45),
+  },
+  {
+    title: "English",
+    allDay: false,
+    start: new Date(2026, 4, 6, 9, 0),
+    end: new Date(2026, 4, 6, 9, 45),
+  },
+  {
+    title: "Biology",
+    allDay: false,
+    start: new Date(2026, 4, 6, 10, 0),
+    end: new Date(2026, 4, 6, 10, 45),
+  },
+  {
+    title: "Physics",
+    allDay: false,
+    start: new Date(2026, 4, 6, 11, 0),
+    end: new Date(2026, 4, 6, 11, 45),
+  },
+
+  {
+    title: "History",
+    allDay: false,
+    start: new Date(2026, 4, 7, 14, 0),
+    end: new Date(2026, 4, 7, 14, 45),
+  },
+  {
+    title: "Math",
+    allDay: false,
+    start: new Date(2026, 4, 7, 8, 0),
+    end: new Date(2026, 4, 7, 8, 45),
+  },
+  {
+    title: "Biology",
+    allDay: false,
+    start: new Date(2026, 4, 7, 10, 0),
+    end: new Date(2026, 4, 7, 10, 45),
+  },
+
+  {
+    title: "Chemistry",
+    allDay: false,
+    start: new Date(2026, 4, 7, 13, 0),
+    end: new Date(2026, 4, 7, 13, 45),
+  },
+  {
+    title: "History",
+    allDay: false,
+    start: new Date(2026, 4, 7, 14, 0),
+    end: new Date(2026, 4, 7, 14, 45),
+  },
+  {
+    title: "English",
+    allDay: false,
+    start: new Date(2026, 4, 7, 9, 0),
+    end: new Date(2026, 4, 7, 9, 45),
+  },
+  {
+    title: "Biology",
+    allDay: false,
+    start: new Date(2026, 4, 7, 10, 0),
+    end: new Date(2026, 4, 7, 10, 45),
+  },
+  {
+    title: "Physics",
+    allDay: false,
+    start: new Date(2026, 4, 7, 11, 0),
+    end: new Date(2026, 4, 7, 11, 45),
+  },
+
+  {
+    title: "History",
+    allDay: false,
+    start: new Date(2026, 4, 8, 14, 0),
+    end: new Date(2026, 4, 8, 14, 45),
+  },
+  {
+    title: "Math",
+    allDay: false,
+    start: new Date(2026, 4, 8, 8, 0),
+    end: new Date(2026, 4, 8, 8, 45),
+  },
+  {
+    title: "English",
+    allDay: false,
+    start: new Date(2026, 4, 8, 9, 0),
+    end: new Date(2026, 4, 8, 9, 45),
+  },
+
+  {
+    title: "Physics",
+    allDay: false,
+    start: new Date(2026, 4, 8, 11, 0),
+    end: new Date(2026, 4, 8, 11, 45),
+  },
+  {
+    title: "Chemistry",
+    allDay: false,
+    start: new Date(2026, 4, 8, 13, 0),
+    end: new Date(2026, 4, 8, 13, 45),
+  },
+  {
+    title: "History",
+    allDay: false,
+    start: new Date(2026, 4, 8, 14, 0),
+    end: new Date(2026, 4, 8, 14, 45),
+  },
+];
