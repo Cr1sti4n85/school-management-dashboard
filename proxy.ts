69bf8e91fb34321d56585e896931b2748fceabe2
Cristian Perez <cperezlecaros@gmail.com>
2026-05-26 21:03:39 -0700

    feat: implement clerk middleware based on role

--- proxy.ts
+++ proxy.ts
@@ -7,15 +7,23 @@ const matchers = Object.keys(routeAccessMap).map((route) => ({
   allowedRoles: routeAccessMap[route],
 }));
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  // if (isProtectedRoute(req)) await auth.protect();
+
+  const { sessionClaims } = await auth();
+  const role = (sessionClaims?.metadata as { role: string })?.role;
+
+  for (const { matcher, allowedRoles } of matchers) {
+    if (matcher(req) && !allowedRoles.includes(role)) {
+      return NextResponse.redirect(new URL(`/${role}`, req.url));
+    }
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for Clerk's auto-proxy path
     "/__clerk/(.*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
