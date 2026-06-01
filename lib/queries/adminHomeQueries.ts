235942ba4c9e7140be5d115f6ae170766e224e78
Cristian Perez <cperezlecaros@gmail.com>
2026-06-01 09:59:13 -0700

    feat: implement students count on admin charts

--- lib/queries/adminHomeQueries.ts
+++ lib/queries/adminHomeQueries.ts
@@ -12,3 +12,19 @@ export const getCardsData = async (role: RoleTypes) => {
   const count = await modelType[role].count();
   return count;
 };
+
+export const getBoysAndGirlsCount = async () => {
+  const data = await prisma.student.groupBy({
+    by: ["sex"],
+    _count: true,
+  });
+
+  const boys = data.find((b) => b.sex === "MALE")?._count || 0;
+  const girls = data.find((b) => b.sex === "FEMALE")?._count || 0;
+
+  return {
+    boys,
+    girls,
+    total: boys + girls,
+  };
+};
