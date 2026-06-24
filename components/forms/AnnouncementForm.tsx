2da7cc85df621d106f0692906b793cf29359b2d5
Cristian Perez <cperezlecaros@gmail.com>
2026-06-24 01:39:30 -0700

    update: add id fields to announcements, events and parent pages

--- components/forms/AnnouncementForm.tsx
+++ components/forms/AnnouncementForm.tsx
@@ -87,6 +87,16 @@ const AnnouncementForm = ({ type, setOpen, data, relatedData }: Props) => {
           defaultValue={data?.date.toLocaleDateString()}
           error={errors.date}
         />
+        {data && (
+          <InputField
+            label="Id"
+            name="id"
+            defaultValue={data?.id?.toString()}
+            register={register}
+            error={errors?.id}
+            hidden
+          />
+        )}
         <div className="flex flex-col gap-2 w-full md:w-1/4">
           <label className="text-xs text-gray-500">Salón</label>
           <select
